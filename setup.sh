2bcc0008f0399e06578fa37971e8aa727d13108f
ntsk <ridha.h2a26@gmail.com>
2018-09-06 20:55:09 +0900

    rename .zfile -> zfile

--- setup.sh
+++ setup.sh
@@ -1,9 +1,22 @@
  #!/bin/zsh
 
-DOT_FILES=(.vim .vimrc .zshrc)
+VIM_FILES=(.vim .vimrc)
+echo "[Setup vim]"
+for file in ${VIM_FILES[@]}
+  do
+      ln -s $HOME/dotfiles/$file $HOME/$file
+  done
 
-for file in ${DOT_FILES[@]}
-do
-    ln -s $HOME/dotfiles/$file $HOME/$file
-done
+ZSH_FILES=(zlogin zlogout zpreztorc zprofile zshenv zshrc)
+echo "[Setup zsh + prezto]"
+if [ -e "$HOME/.zprezto/runcoms" ]; then
+  for file in ${ZSH_FILES[@]}
+    do
+      rm -f $HOME/.zprezto/runcoms/$file
+      ln -s $HOME/dotfiles/$file $HOME/.zprezto/runcoms/$file
+      ln -s $HOME/.zprezto/runcoms/$file $HOME/.$file
+    done
+else
+  echo "Please install prezto before setup !"
+fi
 
