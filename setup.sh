7d2f68d7abe75b115408c6d71f04a89f5f3234cf
ntsk <ridha.h2a26@gmail.com>
2019-01-31 15:11:21 +0900

    Install prezto & prezto-prompt-simple

--- setup.sh
+++ setup.sh
@@ -10,6 +10,10 @@ brew upgrade
 brew bundle
 brew cleanup
 
+# Install prezto & prezto-prompt-simple
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+curl https://raw.githubusercontent.com/kami-zh/prezto-prompt-simple/master/prompt_simple_setup > ~/.zprezto/modules/prompt/functions/prompt_simple_setup
+
 # Generate symlink
 DOT_FILES=(.config .gitconfig .tigrc .peco)
 echo "[Generate symlink]"
