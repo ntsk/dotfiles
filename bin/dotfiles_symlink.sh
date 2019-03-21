0d9b52da938f4bbde3137901513b60bd042bce60
ntsk <ridha.h2a26@gmail.com>
2019-03-22 03:34:17 +0900

    Add symlink for tmux

--- bin/dotfiles_symlink.sh
+++ bin/dotfiles_symlink.sh
@@ -2,5 +2,6 @@
 
 ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/.tigrc $HOME/.tigrc
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -s $HOME/dotfiles/peco $HOME/.peco
 ln -s $HOME/dotfiles/nvim $HOME/.config
