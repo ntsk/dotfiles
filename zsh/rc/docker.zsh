bc914b6201f25d1f6ab30cbe62dc0d5f4718755f
ntsk <ridha.h2a26@gmail.com>
2019-08-01 22:41:42 +0900

    Add docker rmi with peco

--- zsh/rc/docker.zsh
+++ zsh/rc/docker.zsh
@@ -10,6 +10,10 @@ function docker-kill-all() {
   docker kill $(docker ps -q)
 }
 
+function docker-rmi() {
+  docker rmi `docker images | tail -n +2 | peco | awk '{print $3}'`
+}
+
 function docker-logs() {
   docker logs `docker-ps-names | peco | cut -f 1` --follow
 }
