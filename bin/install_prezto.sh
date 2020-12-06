5cf36e09806f5d5e07cf28ca922464466c177aa2
ntsk <ridha.h2a26@gmail.com>
2020-12-07 00:29:28 +0900

    Clean up environment

--- bin/install_prezto.sh
+++ bin/install_prezto.sh
@@ -2,4 +2,4 @@
 
 # Install prezto & prezto-prompt-simple
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-curl https://raw.githubusercontent.com/kami-zh/prezto-prompt-simple/master/prompt_simple_setup > ~/.zprezto/modules/prompt/functions/prompt_simple_setup
+curl -k https://raw.githubusercontent.com/kami-zh/prezto-prompt-simple/master/prompt_simple_setup > ~/.zprezto/modules/prompt/functions/prompt_simple_setup
