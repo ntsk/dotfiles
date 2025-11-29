3d0c9fca970a960a1d8165179a05134df6b952b6
ntsk <ntsk@ntsk.jp>
2025-11-29 20:33:31 +0900

    Migrate completion from ddc.vim to nvim-cmp
    - Replace ddc.vim and denops dependencies with nvim-cmp
    - Add lspkind.nvim for completion icons
    - Configure Tab/S-Tab navigation
    - Add cmdline completion for / ? and :

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,41 +1,91 @@
 return {
   {
-    "vim-denops/denops.vim",
-    lazy = false,
-  },
-  {
-    "Shougo/ddc.vim",
-    event = "InsertEnter",
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
-      "vim-denops/denops.vim",
-      "Shougo/ddc-around",
-      "Shougo/ddc-source-lsp",
-      "Shougo/ddc-matcher_head",
-      "Shougo/ddc-sorter_rank",
-      "Shougo/ddc-ui-native",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-calc",
+      "onsails/lspkind.nvim",
     },
     config = function()
-      vim.fn["ddc#custom#patch_global"]("ui", "native")
-      vim.fn["ddc#custom#patch_global"]("sources", { "lsp", "around" })
-      vim.fn["ddc#custom#patch_global"]("sourceOptions", {
-        _ = {
-          matchers = { "matcher_head" },
-          sorters = { "sorter_rank" },
-        },
-        around = {
-          mark = "around",
-        },
-        lsp = {
-          mark = "lsp",
-          forceCompletionPattern = [[\.\w*|:\w*|->\w*]],
+      local cmp = require("cmp")
+      local lspkind = require("lspkind")
+
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "path" },
+          { name = "calc" },
+        }, {
+          { name = "buffer" },
+        }),
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          format = function(entry, vim_item)
+            local kind = lspkind.cmp_format({
+              mode = "symbol_text",
+              maxwidth = 50,
+              ellipsis_char = "...",
+            })(entry, vim_item)
+            local strings = vim.split(kind.kind, "%s", { trimempty = true })
+            kind.kind = (strings[1] or "") .. " "
+            kind.menu = ({
+              nvim_lsp = "[LSP]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+              calc = "[Calc]",
+              cmdline = "[Cmd]",
+            })[entry.source.name] or ""
+            return kind
+          end,
         },
       })
-      vim.fn["ddc#enable"]()
+
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
     end,
   },
-  { "Shougo/ddc-around", lazy = true },
-  { "Shougo/ddc-source-lsp", lazy = true },
-  { "Shougo/ddc-matcher_head", lazy = true },
-  { "Shougo/ddc-sorter_rank", lazy = true },
-  { "Shougo/ddc-ui-native", lazy = true },
+  { "hrsh7th/cmp-nvim-lsp", lazy = true },
+  { "hrsh7th/cmp-buffer", lazy = true },
+  { "hrsh7th/cmp-path", lazy = true },
+  { "hrsh7th/cmp-cmdline", lazy = true },
+  { "hrsh7th/cmp-calc", lazy = true },
+  { "onsails/lspkind.nvim", lazy = true },
 }
