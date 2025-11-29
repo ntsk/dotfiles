de25ee234953164a9183857c7d165799d554a158
ntsk <ntsk@ntsk.jp>
2025-11-29 20:33:38 +0900

    Update LSP config for Neovim 0.11 native API
    - Use mason-org/mason-lspconfig.nvim with automatic_enable
    - Configure vim.lsp.config for capabilities and lua_ls settings
    - Remove manual server setup in favor of automatic detection

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,19 +1,37 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
+    "mason-org/mason-lspconfig.nvim",
+    lazy = false,
     dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
+      { "mason-org/mason.nvim", build = ":MasonUpdate", opts = {} },
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
       require("mason").setup()
-      require("mason-lspconfig").setup()
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      if ok then
+        capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+      end
+
+      require("mason-lspconfig").setup({
+        automatic_enable = true,
+      })
+
+      vim.lsp.config("*", { capabilities = capabilities })
+
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+
     end,
-  },
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    build = ":MasonUpdate",
   },
 }
