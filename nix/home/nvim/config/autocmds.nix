c12a58ae11767fab1ba2d0d6648f3d1f4158c156
ntsk <ntsk@ntsk.jp>
2026-05-05 23:26:18 +0900

    Stub locals-scope predicates for tree-sitter queries

--- nix/home/nvim/config/autocmds.nix
+++ nix/home/nvim/config/autocmds.nix
@@ -18,6 +18,14 @@
 
     vim.treesitter.language.register("c_sharp", "cs")
 
+    -- Stub locals-scope predicates from nvim-treesitter that some grammar
+    -- queries reference. Without proper locals.scm analysis we cannot evaluate
+    -- them; return safe defaults so highlighting does not error out.
+    vim.treesitter.query.add_predicate("is?", function() return false end, { force = true, all = false })
+    vim.treesitter.query.add_predicate("is-not?", function() return true end, { force = true, all = false })
+    vim.treesitter.query.add_predicate("has-ancestor?", function() return false end, { force = true, all = false })
+    vim.treesitter.query.add_predicate("has-parent?", function() return false end, { force = true, all = false })
+
     autocmd("FileType", {
       group = mygroup,
       callback = function(args)
