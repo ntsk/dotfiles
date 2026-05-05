47f8811fd9dcdb189baf14a1e5013f27685f46b3
ntsk <ntsk@ntsk.jp>
2026-05-05 22:57:17 +0900

    Install tree-sitter parsers via nix

--- nix/home/nvim/nvim.nix
+++ nix/home/nvim/nvim.nix
@@ -1,8 +1,30 @@
-{ ... }:
+{ pkgs, lib, ... }:
 
 # Basic neovim configuration managed by nix
 # Plugins are managed by lazy.nvim (see nvim/lua/plugins/)
 # LSP servers are managed by Mason
+let
+  # Tree-sitter parsers to install via Nix.
+  # Neovim ships with these parsers built-in (do not list here):
+  #   c, lua, markdown, markdown_inline, query, vim, vimdoc
+  treesitterLanguages = [
+    "python"
+    "ruby"
+    "go"
+    "kotlin"
+    "java"
+    "swift"
+    "rust"
+  ];
+
+  treesitterParsers = pkgs.runCommandLocal "nvim-treesitter-parsers" { } ''
+    mkdir -p $out/parser $out/queries
+    ${lib.concatMapStringsSep "\n" (lang: ''
+      ln -s ${pkgs.tree-sitter-grammars."tree-sitter-${lang}"}/parser $out/parser/${lang}.so
+      ln -s ${pkgs.tree-sitter-grammars."tree-sitter-${lang}"}/queries $out/queries/${lang}
+    '') treesitterLanguages}
+  '';
+in
 {
   imports = [
     ./config/options.nix
@@ -16,5 +38,6 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+    plugins = [ treesitterParsers ];
   };
 }
