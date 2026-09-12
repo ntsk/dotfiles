432ccd75694ba93ed4d05a7f89927f17794accf9
ntsk <ntsk@ntsk.jp>
2026-09-12 15:05:15 +0900

    Use official Nix installer instead of Determinate

--- bin/scripts/install-nix.sh
+++ bin/scripts/install-nix.sh
@@ -4,6 +4,6 @@ set -eu
 if [ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 elif ! command -v nix &> /dev/null; then
-  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+  curl --proto '=https' --tlsv1.2 -sSf -L https://nixos.org/nix/install | NIX_INSTALLER_YES=1 sh -s -- --daemon
   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
